35cf5f93f180642e90655a53b13fa155a88ad250
piotrsosnowski32 <piotr.sosnowski31@gmail.com>
2020-05-15 20:00:20 +0200

    test

--- smart_moving_system.py
+++ smart_moving_system.py
@@ -18,10 +18,10 @@ def smart_move(player_position, player_figure):
 
 def first_move(player_position, player_figure):
     if player_position == 5:
-        a = random.randint(1, 9)
+        ab = random.randint(1, 9)
     else:
-        a = 5
-    return a
+        ab = 5
+    return ab
 
 
 # sprawdzenie pól przeciwnika
